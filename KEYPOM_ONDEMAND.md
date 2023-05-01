b2e94d8784c4b07af3bc14d7d0bca3f6f4b0bc7f
Edewor Onyedika <edwordkaso100@gmail.com>
2023-05-01 18:43:19 +0100

    extended the keypom contract to accept arguments and generate a more specific link drop for a specific user on a DApp

---
+++ KEYPOM_ONDEMAND.md
@@ -0,0 +1,31 @@
+KEYPOM ON-DEMAND
+
+SCENARIO: A user lands on a Trustless E-Commerce site that's built on Web3. The user quickly checks around some vendors and their products. After a careful look, he decides to buy a product from vendor A and proceeds to add this product to his cart. Upon clicking the checkout button, the user is asked to connect his wallet: Sigh 😩😩
+
+The User who has little to no prior experience with Web3 has these problems facing him:
+
+-   He starts to ask, how do I get a Web3 wallet?
+
+-   What wallet is the best choice?
+
+-   What's this seed phrase?
+
+-   How do I get money into my Web3 wallet to interact with a DApp?
+
+This throws off the user into making the necessary research. Which as a result, the user abandons the store cart just to get appropriate answers. A tedious process for the typical Web2 user.
+
+SOLUTION:  Keypom On-Demand. The typical keypom feature is to give the "user a special type of access key that can be turned into a wallet. This key can be embedded within a simple Web2 style link that, once clicked, will allow them to input a desired account ID to be created".
+
+Keypom features allow the user to call methods on a DApp contract upon claiming this access key model. Groundbreaking features honestly 🚀🙌
+
+To extend these features which will give our DApp users an astonishing Web2-like experience, here's what we're doing:
+
+-   They add items to their carts and click checkout.
+
+-   The User pays for the total item price using a Web2 payment gateway.
+
+-   The total amount is sent to an external cloud service "VM or Microservices", which in turn spins up unique keypom Function Call drops with specified arguments like method name, contract method specific arguments, and total amount assets.
+
+-   The generated keypom link drop is returned to the client and is executed by the user.
+
+-   Now every user with zero knowledge of Web3 successfully purchases items on the DApp with little to no knowledge of Web3.
--- pipar-keypom.js
+++ pipar-keypom.js
@@ -7,6 +7,8 @@ const {
     initKeypom,
     getEnv,
     createDrop,
+    addToBalance,
+    withdrawBalance,
     parseNearAmount,
     formatLinkdropUrl
 } = keypom;
@@ -16,8 +18,73 @@ const FUNDER_ACCOUNT_ID = "pipar-alpha-rust-v3.testnet";
 // Pass in the network Id Testnet or Mainnet
 const NETWORK_ID = "testnet";
 
+async function addBalance ({amount}) {
+    const CREDENTIALS_DIR = ".near-credentials";
+    const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+
+    // .near-credentials in the local environment NEAR CLI being used to get the Funder ID secret key
+    let keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+    // Network Config
+    let nearConfig = {
+        networkId: NETWORK_ID,
+        keyStore: keyStore,
+        nodeUrl: `https://rpc.${NETWORK_ID}.near.org`,
+        walletUrl: `https://wallet.${NETWORK_ID}.near.org`,
+        helperUrl: `https://helper.${NETWORK_ID}.near.org`,
+        explorerUrl: `https://explorer.${NETWORK_ID}.near.org`,
+    };
+
+    let near = await connect(nearConfig);
+    const fundingAccount = new Account(near.connection, FUNDER_ACCOUNT_ID)
+
+    // Initialization of Keypom SDK
+    await initKeypom({
+        near: near,
+        network: "testnet"
+    });
+
+    await addToBalance({
+        account: fundingAccount,
+        amountYocto: parseNearAmount(`${amount}`),
+        amountNear: `${amount}`,
+        successUrl: "",
+    })
+}
+
+async function withdrawBal () {
+    const CREDENTIALS_DIR = ".near-credentials";
+    const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
+
+    // .near-credentials in the local environment NEAR CLI being used to get the Funder ID secret key
+    let keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
+
+    // Network Config
+    let nearConfig = {
+        networkId: NETWORK_ID,
+        keyStore: keyStore,
+        nodeUrl: `https://rpc.${NETWORK_ID}.near.org`,
+        walletUrl: `https://wallet.${NETWORK_ID}.near.org`,
+        helperUrl: `https://helper.${NETWORK_ID}.near.org`,
+        explorerUrl: `https://explorer.${NETWORK_ID}.near.org`,
+    };
+
+    let near = await connect(nearConfig);
+    const fundingAccount = new Account(near.connection, FUNDER_ACCOUNT_ID)
+
+    // Initialization of Keypom SDK
+    await initKeypom({
+        near: near,
+        network: "testnet"
+    });
+
+    await withdrawBalance({
+        account: fundingAccount,
+    })
+}
+
 // The create Drop function for pipar smart contract
-async function createPiparDrop() {
+async function createPiparDrop({amount, methodCall, arguments}) {
     const CREDENTIALS_DIR = ".near-credentials";
     const credentialsPath =  path.join(homedir, CREDENTIALS_DIR);
 
@@ -48,21 +115,23 @@ async function createPiparDrop() {
         account: fundingAccount,
         numKeys: 1,
         config: {
-            usesPerKey: 20,
+            usesPerKey: 1,
             dropRoot: "pipar-alpha-rust-v3.testnet"
         },
-        depositPerUseNEAR: "11",
+        depositPerUseNEAR: `${amount}`,
         fcData: {
             methods: [
                 [{
                     receiverId: "pipar-alpha-rust-v3.testnet",
-                    methodName: "get_store_cost",
-                    args: JSON.stringify({}),
-                    attachedDeposit: parseNearAmount("0"),
+                    methodName: `${methodCall}`,
+                    args: JSON.stringify(arguments),
+                    attachedDeposit: parseNearAmount(`${amount}`),
                     // successUrl: "https://www.pipar.xyz/"
                 }]
             ]
         },
+        useBalance: false,
+        successUrl: "https://www.pipar.xyz"
     })
 
     console.log('public keys: ', keys.publicKeys);
@@ -89,5 +158,30 @@ async function createPiparDrop() {
     // return keys
 }
 
-createPiparDrop()
+function create (amount, methodCall, arguments) {
+    let val = {
+        amount: amount,
+        methodCall: methodCall,
+        arguments: arguments,
+    };
+
+    return createPiparDrop(val)
+}
+
+function add (amount) {
+    let val = {
+        amount: amount,
+    };
+
+    return addBalance(val)
+}
+
+function withdraw () {
+    return withdrawBal()
+}
+
+
+// create("10", "get_store_cost", {})
+// add("10")
+// withdraw()
 
